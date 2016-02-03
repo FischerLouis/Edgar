a6f5528c1614b099007697f80574ee4a45ba2563
Louis Fischer <fischer.louis.1@gmail.com>
2016-02-03 13:07:30 +0100

    Edgar v2
    Integration with SoundCloud API v2.

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Edgar - The PorcDigy
 
-Edgar provides you the best of SoundCloud directly to your Google Music account for your best pleasure.
+Edgar provides you the best of SoundCloud directly into your Google Music account for your best pleasure.
 
 Edgar duties consist in:
 - Checking everyday (cronjob) your SoundCloud liked songs and playlists,
 - Downloading the new entries (songs and metadata),
 - Uploading the new entries to your Google Music account,
 - Editing the new entries (including song front cover),
-- Building a weekly web report (cronjob) of its activities.
+- Building a weekly web report (cronjob) of its activities (script To Be Updated).
 
 # Remarks
 
@@ -16,7 +16,7 @@ Edgar duties consist in:
 edgar
 │   edgar.py
 │   edgar_reporting.py  
-│   downloaded_songs_list.json    
+│   favorites_uploaded.json    
 │
 └───songs
     │
@@ -29,8 +29,8 @@ edgar
     ├───_old
     │   │
 ```
-- Edgar keeps track of his work in the downloaded_songs_list.json file which can be initialized with and empty json object for instance.
+- Edgar keeps track of his work in the favorites_uploaded.json file which can be initialized with and empty json object for instance.
 
-- SoundCloud does not, at this time, provide the list of liked songs AND playlists (available in beta only). As a result, BeautifulSoup is used to get this list from the web interface.
+- SoundCloud does not, at this time, provide the list of liked songs AND playlists deom the official API. As a result, the API v2 (still in Beta) is used.
 
-- Edgar reports his weekly work on a webpage that needs to be setup.
+- Edgar reports his weekly work on a webpage that needs to be setup (script outdated).
--- edgar.py
+++ edgar.py
@@ -7,88 +7,124 @@ import urllib
 import logging
 import requests
 from logging.handlers import RotatingFileHandler
-from bs4 import BeautifulSoup
 try: import simplejson as json
 except ImportError: import json
 import datetime
 
-###### FUNCTION: UPDATE RECAP ######
-def update_download_reports(status): #CALLED FIRST
-	global reports
-	if status == 0: #DOWNLOAD SUCCESS
-		reports += u'{"download_status":"Success",'
-	if status == 1: #DOWNLOAD ERROR
-		reports += u'{"download_status":"Error",'
-	return;
-	
-def update_upload_reports(status, id): #CALLED SECOND
-	global reports
-	if status == 0: #UPLOAD SUCCESS
-		reports += u'"id":"' + id + '",'
-		reports += u'"upload_status":"Success"' + u'},'
-	if status == 1: #UPLOAD ERROR
-		reports += u'"id":"' + id + '",'
-		reports += u'"upload_status":"Error"' + u'},'
-	return;
+###################################################
+###												###
+###					FUNCTIONS					###
+###												###
+###################################################
 
-###### FUNCTION: YOUTUBE-DL DOWNLOAD HOOK ######
-def dl_hook(d):
-	if d['status'] == 'finished':
-		update_download_reports(0)
+### FUNCTION: YOUTUBE-DL DOWNLOAD HOOK ###
+#TRACK REPORTING ERROR
+def dl_hook_track(d):
 	if d['status'] == 'error':
-		update_download_reports(1)
+		cur_id = d['filename'].split('/')[1].split('.')[0] #GET ID FROM FILE NAME
+		if(cur_id not in reporting_tracks_error):
+			reporting_tracks_error.append(cur_id)
+	return;
+#PLAYLIST REPORTING ERROR
+def dl_hook_playlist(d):
+	if d['status'] == 'error':
+		cur_id = d['filename'].split('/')[1].split('.')[0] #GET ID FROM FILE NAME
+		if(cur_id not in reporting_playlists_error):
+			reporting_playlists_error.append(cur_id)
 	return;
 
-###### FUNCTION: YOUTUBE-DL DOWNLOAD ######
-def download_and_edit_song( id, artist, title, album, url, cover_url, cover_file ):
+### FUNCTION: YOUTUBE-DL DOWNLOAD ###
+def download_upload(id, artist, title, album, url, cover_url, type, parent_id):
+	
+	#COVER DOWNLOAD
+	cover_url = cover_url.replace('https://','http://') #AVOID SSL ISSUES
+	cover_file = 'covers/' + str(id) + '.jpg'
+	urllib.urlretrieve(cover_url, cover_file)#Save cover for future use
+
+	#AUDIO PARAMS
 	audio_extension = u'.mp3'
 	
 	#YOUTUBE-DL PARAMETERS
-	output_tag = 'songs/' + artist + ' - ' + title + audio_extension
-	options = {
-		'format': 'bestaudio/best', # choice of quality
-		'extractaudio' : True,      # only keep the audio
-		'audioformat' : "mp3",      # convert to mp3 
-		'outtmpl': output_tag,      # name the file the ID of the video
-		'noplaylist' : True,       # only download single song, not playlist
-		'logger': logger,
-		'progress_hooks': [dl_hook],
-	}
+	output_tag = 'songs/' + str(id) + audio_extension
+	# TRACK PARAMETER
+	if (type == 'track'):
+		options = {
+			'format': 'bestaudio/best', # choice of quality
+			'extractaudio' : True,      # only keep the audio
+			'audioformat' : "mp3",      # convert to mp3 
+			'outtmpl': output_tag,      # name the file the ID of the video
+			'noplaylist' : True,       # only download single song, not playlist
+			'logger': logger,
+			'progress_hooks': [dl_hook_track],
+		}
+	# PLAYLIST PARAMETER
+	elif (type == 'playlist'):
+		options = {
+			'format': 'bestaudio/best', # choice of quality
+			'extractaudio' : True,      # only keep the audio
+			'audioformat' : "mp3",      # convert to mp3 
+			'outtmpl': output_tag,      # name the file the ID of the video
+			'noplaylist' : True,       # only download single song, not playlist
+			'logger': logger,
+			'progress_hooks': [dl_hook_playlist],
+		}
 	
 	#DOWNLOAD
 	with youtube_dl.YoutubeDL(options) as ydl:
 		ydl.download([url])
-		
-	#EDIT TEXT META DATA
+	
+	#EDIT TEXT META DATA (COVER NOT WORKING)
 	cur_song_file = eyed3.load(output_tag)
 	if cur_song_file.tag is None:
 		cur_song_file.tag = eyed3.id3.Tag()
 		cur_song_file.tag.file_info = eyed3.id3.FileInfo(output_tag)
-	cur_song_file.tag.title = title
-	cur_song_file.tag.artist = artist
-	cur_song_file.tag.album = album
-	#EDIT FRONT COVER META DATA
-	cover_data = urllib.urlopen(cover_url).read()
-	cur_song_file.tag.images.set(3,cover_data,"image/jpeg",u"Front cover")
+	cur_song_file.tag.title = unicode(title)
+	cur_song_file.tag.artist = unicode(artist)
+	cur_song_file.tag.album = unicode(album)
 	#SAVE EDIT
 	cur_song_file.tag.save(version=(2, 3, 0))
 	
-	#UPLOAD TO GOOGLE MUSIC
+	#UPLOAD TO GOOGLE MUSIC + EDIT COVER
 	gmusic_feedback = api.upload(output_tag, False)
-	if output_tag in gmusic_feedback[0]:# CHECK IF SUCCESS
-		update_upload_reports(0, id)
-		#Get Gmusic song ID and update cover through WebClient
-		id_gmusic_song = gmusic_feedback[0][output_tag]
+	if output_tag in gmusic_feedback[0]:# SUCCESS
+		#UPDATE COVER
+		id_gmusic_song = gmusic_feedback[0][output_tag]#Get Gmusic song ID and update cover through WebClient
 		logger.debug('id_gmusic_song: ' + id_gmusic_song + ', cover to be updated.')
 		webclient.upload_album_art(id_gmusic_song, cover_file)
-	else:
-		update_upload_reports(1, id)
+		#UPDATE REPORTING AND FAVORITE UPLOADED LIST
+		if(type == 'track'):
+			if(id not in reporting_tracks_success): #Reporting
+				reporting_tracks_success.append(id)
+			if(id in reporting_tracks_error): #Reporting: Remove False Neg
+				reporting_tracks_error.remove(id)
+			if(id not in uploaded_tracks): #Favorite list
+				uploaded_tracks.append(id)
+		elif(type == 'playlist'):
+			if(parent_id not in reporting_playlists_success): #Reporting
+				reporting_playlists_success.append(parent_id)
+			if(parent_id in reporting_playlists_error): #Reporting: Remove False Neg
+				reporting_playlists_error.remove(parent_id)
+			if(parent_id not in uploaded_playlists): #Favorite list
+				uploaded_playlists.append(parent_id)
+	else: # ERROR
+		if(type == 'track'):
+			if(id not in reporting_tracks_error):
+				reporting_tracks_error.append(id)
+		elif(type == 'playlist'):
+			if(parent_id not in reporting_playlists_error):
+				reporting_playlists_error.append(parent_id)
 	return;
-	
-###### SETUP: DATE
+
+###################################################
+###												###
+###						INIT					###
+###												###
+###################################################
+
+### SETUP: DATE
 now = datetime.datetime.now()
 
-###### SETUP: LOGGING
+### SETUP: LOGGING
 # LOGGER (DEBUG)
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -99,169 +135,146 @@ file_handler = RotatingFileHandler('logs/activity.log', 'a', 1000000, 1) #Append
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
-# HANDLER -> console
-#steam_handler = logging.StreamHandler()
-#steam_handler.setLevel(logging.DEBUG)
-#logger.addHandler(steam_handler)
 
-###### SETUP: GOOGLE MUSIC API - MUSICMANAGER
+logger.info('Edgar, reporting for duty ...')
+
+### SETUP: GOOGLE MUSIC API - MUSICMANAGER
 requests.packages.urllib3.disable_warnings()
-oauth_file = 'XXX'
-mac_address = 'XXX'
+oauth_file = '/root/.oauthfile'
+mac_address = 'XXXX'
 api = Musicmanager(True,True,True)
 musicmanager_login_status = api.login(oauth_file, mac_address)
 logger.debug("Musicmanager login status: " + str(musicmanager_login_status))
 
-###### SETUP: GOOGLE MUSIC API - WEBCLIENT
-google_login = 'XXX'
-google_passw = 'XXX'
+### SETUP: GOOGLE MUSIC API - WEBCLIENT
+google_login = 'XXXX'
+google_passw = 'XXXX'
 webclient = Webclient(True, True, True)
 webclient_login_status = webclient.login(google_login, google_passw)
 logger.debug("Webclient login status: " + str(webclient_login_status))
 
-###### BUILD CURRENT SOUNDS LIST AND INFO ######
-#GET LIKES PAGE
-logger.info('Edgar, reporting for duty ...')
-logger.info('Edgar checks songs list on SoundCloud ...')
-soundcloud_baseuri = 'https://soundcloud.com'
-url_likes = 'https://soundcloud.com/bobytox/likes'
-sock = urllib.urlopen(url_likes)
-htmlLikesPage = sock.read()
-sock.close()
-htmlLikesPageSoup = BeautifulSoup(htmlLikesPage, 'html.parser')
-##BUILD CURRENT SONGS LIST
-soundcloud_client = soundcloud.Client(client_id='118b44c6478d97c39871dedbe7d78ca2')
-current_songs_list = u'{"songs":['
-for article in htmlLikesPageSoup.findAll('article'):#Get all articles in likes page
-	if article.parent.name == 'section':#Get only those with parent "section" = Tracks
-		content_url = soundcloud_baseuri + article.a['href']
-		logger.debug('Url received: ' + content_url)
-		id = u'N/A'
-		title = u'N/A'
-		artist = u'N/A'
-		album = u'N/A'
-		cover_url = u'N/A'
-		has_set = u'False'
-		sets_json = u'N/A'
-		#GET DATA
-		if 'sets' in content_url:#Playlist: EP, Album...
-			has_set = u'True'
-			sets_json = u'"sets":['
-			sets = soundcloud_client.get('/resolve', url=content_url)
-			id = sets.id
-			album = sets.title.replace('/','_')
-			for track in sets.tracks:
-				#GET DATA
-				cur_id = track['id']
-				cur_title = track['title'].replace('/','_')
-				cur_artist = track['user']['username'].replace('/','_')
-				cur_album = album
-				cur_url = track['permalink_url']
-				cur_cover_url = track['artwork_url'].replace('https', 'http', 1).replace('large','t500x500',1)#Cover from HTTP in 't500x500' format
-				#BUILD JSON OBJECT
-				sets_json += u'{"id":"' + str(cur_id) + u'",'
-				sets_json += u'"title":"' + cur_title + u'",'
-				sets_json += u'"artist":"' + cur_artist + u'",'
-				sets_json += u'"album":"' + cur_album + u'",'
-				sets_json += u'"url":"' + cur_url + u'",'
-				sets_json += u'"cover_url":"' + cur_cover_url + u'"},'
-			sets_json = sets_json[:-1] #Removes last ","
-			sets_json += u']'
-		else:#Unique track
-			has_set = u'False'
-			track = soundcloud_client.get('/resolve', url=content_url)
-			id = track.id
-			title = track.title.replace('/','_')
-			artist = track.user['username'].replace('/','_')
-			album = artist + ' - SoundCloud' #Default album if track alone
-			cover_url = track.artwork_url.replace('https', 'http', 1).replace('large','t500x500',1)#Cover from HTTP in 't500x500' format
-		#BUILD JSON OBJECT
-		current_songs_list += u'{"id":"' + str(id) + u'",'
-		current_songs_list += u'"album":"' + album + u'",'
-		if has_set == 'False':
-			current_songs_list += u'"title":"' + title + u'",'
-			current_songs_list += u'"artist":"' + artist + u'",'
-			current_songs_list += u'"cover_url":"' + cover_url + u'",'
-		current_songs_list += u'"url":"' + content_url + u'",'
-		current_songs_list += u'"has_set":"' + has_set + u'"'
-		if has_set == 'True':
-			current_songs_list += u',' + sets_json
-		current_songs_list += u'},'
-current_songs_list = current_songs_list[:-1] #Removes last ","
-current_songs_list += u']}'
-logger.debug('Current songs list: ' + current_songs_list)
+### SETUP: SOUNDCLOUD API
+soundcloud_userid = 'XXXX'
+soundcloud_clientid = 'XXXX'
+soundcloud_api_baseurl = 'https://api-v2.soundcloud.com/users/'
+soundcloud_api_favoriteparam = '/likes?limit=10&offset=0&client_id='
+soundcloud_favorites_url = soundcloud_api_baseurl + soundcloud_userid + soundcloud_api_favoriteparam + soundcloud_clientid
+soundcloud_client = soundcloud.Client(client_id = soundcloud_clientid)
 
-###### COMPARE SONGS LIST TO DOWNLOADED SONGS LIST ######
-logger.info('Edgar is checking which songs need to be downloaded ... ')
-current_songs_list = json.loads(current_songs_list)
+### SETUP: REPORTING DATA
+reporting_tracks_success = []
+reporting_tracks_error = []
+reporting_playlists_success = []
+reporting_playlists_error = []
 
-#OPEN ALREADY DOWNLOADED SONGS LIST
-with open('downloaded_songs_list.json') as json_file:
-    downloaded_songs_list = json.load(json_file)
-logger.debug('Downloaded songs list: ' + json.dumps(downloaded_songs_list))
-#COMPARE LISTS AND BUILD SONGS TO DOWNLOAD LIST
-songs_to_download_list = u'{"songs":['
-download_counter = 0
-for cur_song in current_songs_list['songs']:
-	downloaded = False
-	for downloaded_song in downloaded_songs_list['songs']:
-		if cur_song['id'] == downloaded_song['id']:
-			downloaded = True
-	if downloaded == False:
-		songs_to_download_list += json.dumps(cur_song)
-		songs_to_download_list += u','
-		download_counter += 1
-if download_counter == 0:
-	logger.debug('No new songs.')
-	logger.info('Edgar is afraid his work here is done...')
-	output = u'{"output":{"reports":[],"songs":[]}}'
-else:
-	songs_to_download_list = songs_to_download_list[:-1] #Removes last ","	
-	songs_to_download_list += u']}'
-	logger.debug('Songs to download: ' + songs_to_download_list)
+###################################################
+###												###
+###						START					###
+###												###
+###################################################
 
-	###### DOWNLOAD, EDIT & UPLOAD SONGS ######
-	logger.info('Edgar is starting to download ... ')
-	songs_to_download_list = json.loads(songs_to_download_list)
-	#PREPARE RECAP
-	reports = u'"reports":['
-	#PROCESS SONGS
-	for songs_to_download in songs_to_download_list['songs']:
-		#CHECK IF SET
-		dl_has_sets = songs_to_download['has_set']
-		if dl_has_sets == u'False':
-			#SONG INFO
-			dl_id = songs_to_download['id']
-			dl_artist = songs_to_download['artist']
-			dl_title = songs_to_download['title']
-			dl_album = songs_to_download['album']
-			dl_url = songs_to_download['url']
-			dl_cover_url = songs_to_download['cover_url']
-			dl_cover_file = 'covers/' + str(dl_id) + '_' + dl_artist + ' - ' + dl_title + '.jpg'
-			urllib.urlretrieve(dl_cover_url, dl_cover_file)#Save cover for future use
-			download_and_edit_song(dl_id, dl_artist, dl_title, dl_album, dl_url, dl_cover_url, dl_cover_file)
-		else:
-			for songs_from_set_to_download in songs_to_download[u'sets']:
-				#SONG INFO
-				dl_id = songs_from_set_to_download['id']
-				dl_artist = songs_from_set_to_download['artist']
-				dl_title = songs_from_set_to_download['title']
-				dl_album = songs_from_set_to_download['album']
-				dl_url = songs_from_set_to_download['url']
-				dl_cover_url = songs_from_set_to_download['cover_url']
-				dl_cover_file = 'covers/' + str(dl_id) + '_' + dl_artist + ' - ' + dl_title + '.jpg'
-				urllib.urlretrieve(dl_cover_url, dl_cover_file)#Save cover for future use
-				download_and_edit_song(dl_id, dl_artist, dl_title, dl_album, dl_url, dl_cover_url, dl_cover_file)
-	#GENERATE OUTPUT FOR REPORTING
-	reports = reports[:-1] #Removes last ","
-	reports += u']'
-	output = u'{"output":{' + reports + ',' + json.dumps(songs_to_download_list)[1:][:-1] + '}}'
-output_filename = 'reports/report_' + now.strftime("%Y-%m-%d") + '.json'
-with open(output_filename, 'w') as output_file:
-	json.dump(output, output_file)
-#REPLACE OLD LIST WITH NEW ONE (downloaded_songs_list = current_songs_list)
-with open('downloaded_songs_list.json', 'w') as outfile:
-	json.dump(current_songs_list, outfile)
-#LOG OUT
+### DOWNLOAD & UPLOAD NEW TRACKS/PLAYLISTS ######
+logger.info('Edgar checks songs list on SoundCloud API v2 ...')
+
+#GET CURRENT FAVORITES (TRACK + PLAYLIST)
+uploaded_tracks = []
+uploaded_playlists = []
+with open('favorites_uploaded.json') as file_favorites_uploaded:
+    json_favorites_uploaded = json.load(file_favorites_uploaded)
+logger.debug('OUTPUT '+ json.dumps(json_favorites_uploaded))
+for uploaded_track in json_favorites_uploaded['favorites_uploaded']['tracks']:
+	uploaded_tracks.append(uploaded_track['id'])
+for uploaded_playlist in json_favorites_uploaded['favorites_uploaded']['playlists']:
+	uploaded_playlists.append(uploaded_playlist['id'])
+
+#GET CURRENT FAVORITES (TRACK + PLAYLIST)
+soundcloud_favorites_data = urllib.urlopen(soundcloud_favorites_url)
+soundcloud_favorites_json = json.loads(soundcloud_favorites_data.read())
+
+for favorite in soundcloud_favorites_json['collection']:
+	if(favorite.get('track')):
+		if (favorite['track']['id'] not in uploaded_tracks): ###LIVE: in >>> not in ###
+			cur_track = favorite['track']
+			cur_id = cur_track['id']
+			cur_artist = cur_track['user']['username']
+			cur_title = cur_track['title']
+			cur_album = cur_track['user']['username'] + ' - SoundCloud'
+			cur_url =  cur_track['uri']
+			cur_cover_url = cur_track['artwork_url'].replace('-large','-t500x500') #Max size image
+			logger.debug('COVER_URL TO DOWNLOAD: '+ cur_cover_url)
+			cur_type = 'track'
+			download_upload(cur_id, cur_artist, cur_title, cur_album, cur_url, cur_cover_url, cur_type, cur_id)
+	elif (favorite.get('playlist')):
+		if (favorite['playlist']['id'] not in uploaded_playlists): ###LIVE: in >>> not in ###
+			cur_playlist = favorite['playlist']
+			cur_playlist_id = cur_playlist['id']
+			cur_playlist_url = cur_playlist['uri']
+			cur_playlist_cover_url = cur_playlist['artwork_url'].replace('-large','-t500x500') #Max size image
+			logger.debug('PLAYLIST_COVER_URL TO DOWNLOAD: '+ cur_playlist_cover_url)
+			cur_album = cur_playlist['title']
+			cur_type = 'playlist'
+			cur_playlist_details = soundcloud_client.get('/resolve', url=cur_playlist_url)
+			for cur_track in cur_playlist_details.tracks:
+				cur_id = cur_track['id']
+				cur_artist = cur_track['user']['username']
+				cur_title = cur_track['title']
+				cur_url =  cur_track['uri']
+				cur_cover_url = cur_track['artwork_url'] #Might be NoneType
+				if(cur_cover_url is None): # If track with no cover, takes cover from playlist
+					cur_cover_url = cur_playlist_cover_url
+				else:
+					cur_cover_url = cur_cover_url.replace('-large','-t500x500') #Max size image
+				logger.debug('PLAYLIST_TRACK_COVER_URL TO DOWNLOAD: '+ cur_cover_url)
+				download_upload(cur_id, cur_artist, cur_title, cur_album, cur_url, cur_cover_url, cur_type, cur_playlist_id)
+
+### BUILD REPORTING JSON FILE
+reporting_json = u'{"tracks":'
+reporting_json = reporting_json + u'{"success":['
+for success_track in reporting_tracks_success:
+	reporting_json = reporting_json + str(success_track) + ','
+if (len(reporting_tracks_success) > 0):
+	reporting_json = reporting_json[:-1] #Removes last ","
+reporting_json = reporting_json + u'], "error":['
+for error_track in reporting_tracks_error:
+	reporting_json = reporting_json + str(error_track) + ','
+if (len(reporting_tracks_error) > 0):
+	reporting_json = reporting_json[:-1] #Removes last ","
+reporting_json = reporting_json + u']}, "playlists":'
+reporting_json = reporting_json + u'{"success":['
+for success_playlist in reporting_playlists_success:
+	reporting_json = reporting_json + str(success_playlist) + ','
+if (len(reporting_playlists_success) > 0):
+	reporting_json = reporting_json[:-1] #Removes last ","
+reporting_json = reporting_json + u'], "error":['
+for error_playlist in reporting_playlists_error:
+	reporting_json = reporting_json + str(error_playlist) + ','
+if (len(reporting_playlists_error) > 0):
+	reporting_json = reporting_json[:-1] #Removes last ","
+reporting_json = reporting_json + u']}}'
+logger.debug('REPORTING JSON: ' + str(reporting_json))
+
+reporting_json_filename = 'reports/report_' + now.strftime("%Y-%m-%d") + '.json'
+with open(reporting_json_filename, 'w') as output_file:
+	output_file.write(reporting_json)
+
+### UPDATE favorites_uploaded JSON
+updated_favorites = u'{"favorites_uploaded": {"tracks": ['
+for track in uploaded_tracks:
+	updated_favorites = updated_favorites + '{"id": ' + str(track) + '},'
+if (len(uploaded_tracks) > 0):
+	updated_favorites = updated_favorites[:-1] #Removes last ","
+updated_favorites = updated_favorites + u'], "playlists": ['
+for playlist in uploaded_playlists:
+	updated_favorites = updated_favorites + '{"id": ' + str(playlist) + '},'
+if (len(uploaded_playlists) > 0):
+	updated_favorites = updated_favorites[:-1] #Removes last ","
+updated_favorites = updated_favorites + u']}}'
+
+logger.debug('UPDATED UPLOADED FAVORITES: ' + str(updated_favorites))
+
+with open('favorites_uploaded.json', 'w') as outfile:
+	outfile.write(updated_favorites)
+
+### LOG OUT
 webclient.logout()
 logger.info('Done! Edgar out!')
